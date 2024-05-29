44d2e2e2e4bd9517769b49d2dd2754d3ea69c64a
1s2r3i4n5u <169056492+1s2r3i4n5u@users.noreply.github.com>
2024-05-29 20:47:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -77,8 +77,8 @@ resource "aws_security_group" "python_sg" {
   }
  # Car-prediction access from anywhere
  ingress {
-   from_port   = 8000
-   to_port     = 8000
+   from_port   = 7000
+   to_port     = 7000
    protocol    = "tcp"
    cidr_blocks = ["0.0.0.0/0"]
  }
